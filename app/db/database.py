21b42af5516c70f3908980523e2747d6e7534204
iXtazia <nicolas.boulogne-curriez@epitech.eu>
2026-03-11 14:18:31 +0800

    [ADD] Database connected

---
+++ app/db/database.py
@@ -0,0 +1,20 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import certifi
+
+from app.core.config import MONGO_URI
+
+client = MongoClient(
+    MONGO_URI,
+    server_api=ServerApi("1"),
+    tls=True,
+    tlsCAFile=certifi.where()
+)
+
+try:
+    client.admin.command("ping")
+    print("Connected successfully!")
+except Exception as e:
+    print("Error:", e)
+
+db = client["items"]
--- app/routers/item.py
+++ app/routers/item.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 from app.models.item import Item
+from app.db.database import db
 
 router = APIRouter(prefix="/item", tags=["item"])
+
+@router.post("/item/{item_id}")
+def create_item(item: dict):
+    result = db["items"].insert_one(item)
+    return {"inserted_id": str(result.inserted_id)}
 
 @router.get("/item/{item_id}")
 def read_item(item_id: int, q: str | None = None):
